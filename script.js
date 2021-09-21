7dd07e96504b04e1fb9a8278ca4cc67dfc4cfddc
Allan Pennycuff <allanp1238@gmail.com>
2021-09-21 08:05:41 -0500

    Completed JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(13);
+// Numbers, Strings, Booleans, Null, and Undefined
+// + - * / ** %
+console.log(5%2);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk"+"eye");
+console.log("Allan".length+"Pennycuff".length);
+console.log("Avengers: Endgame"[16]);
