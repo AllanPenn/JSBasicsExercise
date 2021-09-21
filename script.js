af6765d2d01ba4b2f04acd4ce04f9bbde2f30a66
Allan Pennycuff <allanp1238@gmail.com>
2021-09-21 09:52:16 -0500

    Completed the bonus for JSBasicsExercise

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ console.log(13);
 // Numbers, Strings, Booleans, Null, and Undefined
 // + - * / ** %
 console.log(5%2);
+console.log(5%4);
 console.log('I\'M MARY POPPINS, Y\'ALL!');
 console.log("Hawk"+"eye");
 console.log("Allan".length+"Pennycuff".length);
 console.log("Avengers: Endgame"[16]);
+
+// BONUS
+console.log("\\");
+console.log(5 ** 4 - 9);
+console.log(("I".length + "Am".length) * "Iron".length / "Man".length % 4);
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+var m = 25;
+var c = 4;
+var u = m + c;
+console.log(u);
